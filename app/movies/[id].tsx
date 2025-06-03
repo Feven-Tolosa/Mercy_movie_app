d9e7d20ca8b3d6479bf96a8f14eb7f49ea40cb14
Feven-Tolosa <tolosafeven0@gmail.com>
2025-06-03 21:57:52 +0300

    movie details

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import useFetch from '@/services/useFetch'
+import { fetchMovieDetails } from '@/services/api'
+import { icons } from '@/constants/icons'
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams()
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  )
   return (
-    <View>
-      <Text className='text-5xl top-10 left-2 font-bold text-accent'>
-        MovieDetails
-      </Text>
+    <View className='bg-primary fle-1'>
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <view>
+          <Image
+            source={{
+              uri: `https//image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className='w-full h-[550px]'
+            resizeMode='stretch'
+          />
+        </view>
+        <View className='flex-col items-start justify-center mt-5 px-5'>
+          <Text className='text-light-200 text-sm'>
+            {movie?.release_date?.split('-')[0]}
+          </Text>
+          <Text className='text-light-200 text-sm'>{movie?.runtime}m</Text>
+        </View>
+        <View className='flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2 '>
+          <Image source={icons.star} className='size-4' />
+          <Text className='text-white font-bold text-sm'>
+            {Math.round(movie?.vote_average ?? 0)}/10
+          </Text>
+          <Text className='text-light-200 text-sm'>
+            ({movie?.vote_count} votes)
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
